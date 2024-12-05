bca8102240533077d32ce0c02a6963c7ea6d7c0c
Lili <lili.berenyi@gmail.com>
2024-12-05 10:06:58 +0000

    Add empty project

---
+++ mars-rover-test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace mars_rover_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
